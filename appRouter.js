49d2bdcacc494342d3c5853a19c40168d8110a80
seba <ssolezzi@gmail.com>
2025-08-27 10:44:34 -0300

    Agregada la ruta para obtner por su numero atomico

--- appRouter.js
+++ appRouter.js
@@ -6,6 +6,8 @@ const router = Router();
 
 router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
 router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
+router.get('/api/v1/atomic-number/:number',getElementBySymbol); //Obtener elemento por su número
+
 
 
 export default router;
