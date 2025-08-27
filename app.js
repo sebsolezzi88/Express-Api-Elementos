aaab89f18e84f6c68bd185ad07b7d2d72e8b4c9d
seba <ssolezzi@gmail.com>
2025-08-27 10:31:36 -0300

    Agregadas rutas al app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import expres from 'express';
-
+import routes from './appRouter.js';
 
 const app  = expres();
+
+//Importando las rutas
+app.use('/',routes);
 
 app.listen(3000,(req,res)=>{
     console.log("Server corriendo en el puerto 3000");
