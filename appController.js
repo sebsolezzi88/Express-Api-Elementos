60d8e178254f47b0be2dffa0240cc08a689aaca3
seba <ssolezzi@gmail.com>
2025-08-27 10:38:38 -0300

    Cambios en loadCSV para cargarlos una sola vez en memoria

--- appController.js
+++ appController.js
@@ -5,6 +5,9 @@ function capitalize(text) {
   return text[0].toUpperCase() + text.slice(1).toLowerCase();
 }
 
+let elementsCache = [];
+
+
 //Cargar Data
 function loadCSV() {
   return new Promise((resolve, reject) => {
@@ -17,8 +20,31 @@ function loadCSV() {
   });
 }
 
+// Al iniciar la app, cargar datos una vez
+(async () => {
+  elementsCache = await loadCSV();
+  console.log("CSV cargado en memoria con", elementsCache.length, "elementos");
+})();
+
+
 //Obtener todos los elementos químicos
 export const getAllElements = async (req, res) => {
+  return res.status(200).json(elementsCache);
+};
+
+//Obtener elemento por su simbolo ej He,O, Fe
+export const getElementBySymbol = async (req, res) => {
   const elements = await loadCSV();
-  return res.status(200).json(elements);
+  let { symbol } = req.params;
+  symbol = capitalize(symbol);
+  
+  const element = elementsCache.filter((element) => element.Symbol === symbol);
+
+  if (element.length === 0) {
+    return res
+      .status(404)
+      .json({ error: "No se encontró ningún registro con ese símbolo" });
+  }
+
+  return res.status(200).json(element);
 };
