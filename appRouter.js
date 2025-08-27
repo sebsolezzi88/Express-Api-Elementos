0367fe5ce62bba388ba481da2bcd0db08b95b48f
seba <ssolezzi@gmail.com>
2025-08-27 19:32:59 -0300

    Agrega la ruta para obtener un elemento por su nombre

--- appRouter.js
+++ appRouter.js
@@ -6,6 +6,7 @@ const router = Router();
 router.get('/',renderIndex); //Renderizar página principal
 router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
 router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
+router.get('/api/v1/name/:name',getElementBySymbol); //Obtener elemento por su nombre
 router.get('/api/v1/atomic-number/:number',getElementByAtomicNumber); //Obtener elemento por su número
 router.get('/api/v1/phase/:phase',getElementsByPhase); //Obtener elementos por su fase
 router.get('/api/v1/type/:type',getElementsByType); //Obtener elementos por su tipo
