ce25abd07cfdb0eae6039a936f92509d4206d07c
seba <ssolezzi@gmail.com>
2025-08-27 10:51:20 -0300

    Ruta para obtener elementos por su tipo

--- appRouter.js
+++ appRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllElements, getElementByAtomicNumber, getElementBySymbol, getElementsByPhse } from "./appController.js";
+import { getAllElements, getElementByAtomicNumber, getElementBySymbol, getElementsByPhse, getElementsByType } from "./appController.js";
 
 
 const router = Router();
@@ -8,6 +8,8 @@ router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
 router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
 router.get('/api/v1/atomic-number/:number',getElementByAtomicNumber); //Obtener elemento por su número
 router.get('/api/v1/phase/:phase',getElementsByPhse); //Obtener elementos por su fase
+router.get('/api/v1/type/:type',getElementsByType); //Obtener elementos por su tipo
+
 
 
 
