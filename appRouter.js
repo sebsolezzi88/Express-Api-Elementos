9182d8f88b54fe0240816b2e0fe0735a8a2d69a1
seba <ssolezzi@gmail.com>
2025-08-27 10:48:38 -0300

    Agregada ruta para obtener elementos por su fase

--- appRouter.js
+++ appRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllElements, getElementByAtomicNumber, getElementBySymbol } from "./appController.js";
+import { getAllElements, getElementByAtomicNumber, getElementBySymbol, getElementsByPhse } from "./appController.js";
 
 
 const router = Router();
@@ -7,6 +7,8 @@ const router = Router();
 router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
 router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
 router.get('/api/v1/atomic-number/:number',getElementByAtomicNumber); //Obtener elemento por su número
+router.get('/api/v1/phase/:phase',getElementsByPhse); //Obtener elementos por su fase
+
 
 
 
