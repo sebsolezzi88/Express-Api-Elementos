dbb2f936cea4373f523f19d51d1083b61d0a7da8
seba <ssolezzi@gmail.com>
2025-08-27 10:41:30 -0300

    Agregada ruta para obtner elemento por su simbolo

--- appRouter.js
+++ appRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { getAllElements } from "./appController.js";
+import { getAllElements, getElementBySymbol } from "./appController.js";
 
 
 const router = Router();
 
-router.get('/api/v1/elements/all',getAllElements);
+router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
+router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
+
 
 export default router;
