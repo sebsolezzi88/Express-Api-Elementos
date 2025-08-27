9b27ed92a006d3b844b08057d208cf843518e2c7
seba <ssolezzi@gmail.com>
2025-08-27 19:34:18 -0300

    Agregada getElementByName a la ruta

--- appRouter.js
+++ appRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { getAllElements, getElementByAtomicNumber, getElementBySymbol, getElementsByGroup, getElementsByPhase, getElementsByType, renderIndex } from "./appController.js";
+import { getAllElements, getElementByAtomicNumber, getElementByName, getElementBySymbol, getElementsByGroup, getElementsByPhase, getElementsByType, renderIndex } from "./appController.js";
 
 const router = Router();
 
 router.get('/',renderIndex); //Renderizar página principal
 router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
 router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
-router.get('/api/v1/name/:name',getElementBySymbol); //Obtener elemento por su nombre
+router.get('/api/v1/name/:name',getElementByName); //Obtener elemento por su nombre
 router.get('/api/v1/atomic-number/:number',getElementByAtomicNumber); //Obtener elemento por su número
 router.get('/api/v1/phase/:phase',getElementsByPhase); //Obtener elementos por su fase
 router.get('/api/v1/type/:type',getElementsByType); //Obtener elementos por su tipo
