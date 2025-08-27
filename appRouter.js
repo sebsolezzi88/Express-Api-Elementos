0a36eb0afe4430523d299ab2c7323decd8f16072
seba <ssolezzi@gmail.com>
2025-08-27 11:19:08 -0300

    Agregada ruta para reenderizar main page

--- appRouter.js
+++ appRouter.js
@@ -1,19 +1,15 @@
 import { Router } from "express";
-import { getAllElements, getElementByAtomicNumber, getElementBySymbol, getElementsByGroup, getElementsByPhse, getElementsByType } from "./appController.js";
-
+import { getAllElements, getElementByAtomicNumber, getElementBySymbol, getElementsByGroup, getElementsByPhse, getElementsByType, renderIndex } from "./appController.js";
 
 const router = Router();
 
+router.get('/',renderIndex); //Renderizar página principal
 router.get('/api/v1/elements/all',getAllElements); //Obtener todos los elementos
 router.get('/api/v1/symbol/:symbol',getElementBySymbol); //Obtener elemento por su simbolo
 router.get('/api/v1/atomic-number/:number',getElementByAtomicNumber); //Obtener elemento por su número
 router.get('/api/v1/phase/:phase',getElementsByPhse); //Obtener elementos por su fase
 router.get('/api/v1/type/:type',getElementsByType); //Obtener elementos por su tipo
 router.get('/api/v1/group/:group',getElementsByGroup); //Obtener elementos por su grupo
-
-
-
-
 
 
 export default router;
