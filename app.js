3170ecef10f7a127d5e1b240be46b644149a2ba2
seba <ssolezzi@gmail.com>
2025-08-27 20:05:40 -0300

    Agrega uso de ruta para archivos estaticos

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-import expres from 'express';
+import express from 'express';
 import routes from './appRouter.js';
+import path from 'path'
+import { fileURLToPath } from 'url';
 
-const app  = expres();
+const app  = express();
+
+//Ruta para archivos estaticos
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "ejs"); //Motor de plantillas
 //Importando las rutas
